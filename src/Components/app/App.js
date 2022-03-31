f61c6a2fae7b915a13734e0109e12ab76dd6c8a4
AinoGG <ryadomgg@gmail.com>
2022-03-31 13:56:18 +0300

    replace files

--- src/Components/app/App.js
+++ src/Components/app/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.scss'
 
 function App() {
   return (
     <div className="App">
-      HEY
+      <div className="hey">
+        Hey
+      </div>
     </div>
   );
 }
